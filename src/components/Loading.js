2a65a492c08f3f32d4dd4dd77f4083777af2ef20
kunapot <kunapot@thinknet.co.th>
2017-07-13 16:09:57 +0700

    correct proptype

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactLoading from 'react-loading'
 
 const Loading = () => (
-  <ReactLoading type="spin" color='#000' delay='0' />
+  <ReactLoading type="spin" color='#000' delay={0} />
 )
 
 export default Loading
